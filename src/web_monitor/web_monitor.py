57cbedf7dd113e812492ee1740d4d7115a6286be
Cy83rr <cy83rr+github@gmail.com>
2018-05-16 22:46:23 +0200

    simplified asynchronous part

--- src/web_monitor/web_monitor.py
+++ src/web_monitor/web_monitor.py
@@ -4,7 +4,6 @@ Creates the logger, processes and threads, initializes everything
 import asyncio
 import logging
 import time
-from contextlib import suppress
 from multiprocessing import Process
 
 import aiohttp
@@ -13,60 +12,16 @@ from src.web_monitor.server import server
 
 
 async def do_request(url, period_ms, content):
-    log = logging.getLogger(__name__)
-    p = Periodic(url, period_ms, content)
-    try:
-        log.debug('Start')
-        await p.start()
-        await asyncio.sleep(3.1)  # it repeats the task as many times it can in the sleep
-
-        log.debug('Stop')
-        await p.stop()
-        await asyncio.sleep(3.1)
-
-        log.debug('Start')
-        await p.start()
-        await asyncio.sleep(3.1)
-    finally:
-        await p.stop()  # we should stop task finally
-
-
-class Periodic:
-    def __init__(self, url, time, content):
-        self.url = url
-        self.time = time
-        self.is_started = False
-        self.content = content
-        self._task = None
-
-    async def start(self):
-        if not self.is_started:
-            self.is_started = True
-            # Start task to call func periodically:
-            self._task = asyncio.ensure_future(self._run())
-
-    async def stop(self):
-        if self.is_started:
-            self.is_started = False
-            # Stop task and await it stopped:
-            self._task.cancel()
-            with suppress(asyncio.CancelledError):
-                await self._task
-
-    async def _run(self):
-        async with aiohttp.ClientSession() as session:
-            log = logging.getLogger(__name__)
-            while True:
-                await asyncio.sleep(self.time / 1000)
-                start_time = time.time()
-                response = await fetch(session, self.url)
-                end_time = time.time()
-                log.info('%s\t%s\t%s', self.url, self.content in response, end_time - start_time)
-
-
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+    #asyncio.ensure_future
+    async with aiohttp.ClientSession() as session:
+        log = logging.getLogger(__name__)
+        while True:
+            await asyncio.sleep(period_ms / 1000)
+            start_time = time.time()
+            response = await session.get(url)
+            text = await response.text()
+            end_time = time.time()
+            log.info('%s\t%s\t%s', url, content in text, end_time - start_time)
 
 
 def start_app(config):
